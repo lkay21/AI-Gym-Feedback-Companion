3096efb331f0a5b4fa6156d9ac4b5c566664f4c9
lkay21 <115806181+lkay21@users.noreply.github.com>
2026-01-28 12:07:40 -0500

    init OOP structure and early biomechanic metrics

--- app/openpose.py
+++ app/openpose.py
@@ -1,12 +1,20 @@
 import cv2 as cv
 import os
 import numpy as np
+import exercise
+import time
+
 
 # body parts and pose pairs for OpenPose
 BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
                "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
                "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }                   
+               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }      
+
+BODY_PARTS_BY_INDEX = { 0: "Nose", 1: "Neck", 2: "RShoulder", 3: "RElbow", 4: "RWrist",
+               5: "LShoulder", 6: "LElbow", 7: "LWrist", 8: "RHip", 9: "RKnee",
+               10: "RAnkle", 11: "LHip", 12: "LKnee", 13: "LAnkle", 14: "REye",
+               15: "LEye", 16: "REar", 17: "LEar", 18: "Background" }             
 
 POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
                ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
@@ -15,11 +23,11 @@ POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElb
                ["Neck", "Nose"], ["Nose", "REye"], ["REye", "REar"],
                ["Nose", "LEye"], ["LEye", "LEar"] ]
 
-def generate_pose(file_path):
+def generate_pose(file_path, joint_group, frame_vals):
 
     # load the pre-trained model
     net = cv.dnn.readNetFromTensorflow("app/graph_opt.pb")
-    thres = 0.2
+    thres = 0.3
 
     # read the video file and use opencv to gather width, height, fps
     cap = cv.VideoCapture(file_path)
@@ -40,6 +48,7 @@ def generate_pose(file_path):
     ret, frame_1 = cap.read()
     ret, frame_2 = cap.read()
 
+    frame_count = 0
     # loop through video frames
     while cap.isOpened():
 
@@ -77,7 +86,18 @@ def generate_pose(file_path):
                 points.append((int(x), int(y)))
                 cv.circle(frame, (int(x), int(y)), 5, (0, 255, 255), thickness=-1, lineType=cv.FILLED)
                 cv.putText(frame, "{}".format(i), (int(x), int(y)), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, lineType=cv.LINE_AA)
-                # print(f"Body Part {i}: ({int(x)}, {int(y)}) with prob {prob}")
+                
+                if i in joint_group:
+                    print(f"Frame {frame_count} - Body Part {BODY_PARTS_BY_INDEX[i]}: ({int(x)}, {int(y)}) with prob {prob}")
+
+                    # frame_vals is dictionary of dictionaries, need to access body_part index first and then frame count
+                    # if frame count does not exist, create it and then add the x,y tuple
+                    if BODY_PARTS_BY_INDEX[i] in frame_vals:
+                        frame_vals[BODY_PARTS_BY_INDEX[i]][frame_count] = (int(x), int(y))
+                    else:
+                        frame_vals[BODY_PARTS_BY_INDEX[i]] = {}
+                        frame_vals[BODY_PARTS_BY_INDEX[i]][frame_count] = (int(x), int(y))
+                    
             else:
                 points.append(None)
 
@@ -98,6 +118,8 @@ def generate_pose(file_path):
         frame_1 = frame_2
         ret, frame_2 = cap.read()   
 
+        frame_count += 1
+
         if not ret:
             break
         
@@ -108,56 +130,39 @@ def generate_pose(file_path):
     out.release()
     cv.destroyAllWindows()
 
-
-# def process_video(file_path, file_name):
-#     cap = cv.VideoCapture(file_path)
-
-#     frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-#     frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-#     fps = cap.get(cv.CAP_PROP_FPS)
-#     fourcc = cv.VideoWriter_fourcc(*'avc1')
-
-#     output_path = os.path.join('static', 'tracked_videos', file_name)
-#     out = cv.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
-
-#     ret, frame_1 = cap.read()
-#     ret, frame_2 = cap.read()
-
-#     while cap.isOpened():
-
-#         diff = cv.absdiff(frame_1, frame_2)
-#         gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
-#         blur = cv.GaussianBlur(gray, (5, 5), 0)
-#         _, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
-#         dilated = cv.dilate(thresh, None, iterations=3)
-#         contours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        
-#         for contour in contours:
-#             if cv.contourArea(contour) < 900:
-#                 continue
-#             (x, y, w, h) = cv.boundingRect(contour)
-#             cv.rectangle(frame_1, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-#         out.write(frame_1)
-
-#         # cv.imshow("Original Frame", frame_1)
-
-#         frame_1 = frame_2
-#         ret, frame_2 = cap.read()   
-
-#         if not ret:
-#             break
-        
-#         if cv.waitKey(40) == 27:
-#             break
-
-#     cap.release()
-#     out.release()
-#     cv.destroyAllWindows()
-
-#     return file_name
+    return frame_count, fps
 
 if __name__ == "__main__":
-    video_path = r"C:\Users\logan\SD\AI-Gym-Feedback-Companion\app\video_in\dance.mp4"
-    # video_path = "AI-Gym-Feedback-Companion/app/video_in/dance.mp4"
-    generate_pose(video_path)
+    left_bicep_curl = exercise.Exercise.from_preset("iso_left_bicep_curl")
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    video_path = os.path.join(script_dir, "video_in", "test.mp4")
+
+    frame_vals = {}
+    joint_group_nums = []
+
+    for joint in left_bicep_curl.joint_group:
+        joint_group_nums.append(exercise.BODY_PARTS[joint])
+        frame_vals[joint] = {}
+
+    for key, val in frame_vals.items():
+        print(f"Initializing frame_vals for {key}: {val}")
+
+    start_time = time.time()
+    frame_count, fps  = generate_pose(video_path, joint_group_nums, frame_vals)
+    end_time = time.time()
+
+    print(f"\nTime taken for pose estimation: {end_time - start_time} seconds")
+
+    print("\nFrames processed for pose estimation of video for Excercise")
+    print(f"Exercise Name: {left_bicep_curl.name}"
+        f"\nIsolated Movement: {left_bicep_curl.isolated_movement}"
+        f"\nJoint Group: {left_bicep_curl.joint_group}\n")
+    
+    left_bicep_curl.set_frame_values(frame_vals, frame_count, fps)
+    left_bicep_curl.graph_metrics()
+    
+    for joint in frame_vals.keys():
+        print(f"Joint: {joint}")
+        print(frame_vals[joint])
+        print("\n")
