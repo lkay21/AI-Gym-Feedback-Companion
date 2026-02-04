5226c4f22ca9b1ad6e83302a85f53b3f9be6f7b3
mythrai <mkapidi@purdue.edu>
2026-02-04 16:04:21 -0500

    workout plan screen

---
+++ app/dashboard.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function dashboard() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Fitness Dashboard</Text>
+    </View>
+  );
+}
