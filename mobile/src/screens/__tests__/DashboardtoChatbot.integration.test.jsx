2b84bd9fffe3395234865d820379e5db24ee835c
mythrai <mkapidi@purdue.edu>
2026-03-15 20:48:38 -0400

    Passing tests

--- mobile/src/screens/__tests__/DashboardtoChatbot.integration.test.jsx
+++ mobile/src/screens/__tests__/DashboardtoChatbot.integration.test.jsx
@@ -1,11 +1,52 @@
+jest.mock("react-native-gesture-handler", () => {
+  const React = require("react");
+  const { View } = require("react-native");
+
+  return {
+    __esModule: true,
+    default: {
+      install: jest.fn(),
+    },
+    GestureHandlerRootView: ({ children }) => <View>{children}</View>,
+    State: {},
+    PanGestureHandler: View,
+    TapGestureHandler: View,
+    LongPressGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    NativeViewGestureHandler: View,
+    RotationGestureHandler: View,
+    PinchGestureHandler: View,
+    Directions: {},
+  };
+});
+
 import React from "react";
 import { Text } from "react-native";
 import { render, fireEvent } from "@testing-library/react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import DashboardScreen from "../DashboardScreen";
 
-const Stack = createNativeStackNavigator();
+jest.mock("@expo/vector-icons", () => {
+  const React = require("react");
+  const { Text } = require("react-native");
+
+  return {
+    Ionicons: ({ name }) => <Text>{name}</Text>,
+  };
+});
+
+jest.mock("../../components/MenuDropdown", () => {
+  const React = require("react");
+  const { Text } = require("react-native");
+
+  return function MockMenuDropdown() {
+    return <Text>Mock MenuDropdown</Text>;
+  };
+});
+
+const Stack = createStackNavigator();
 
 function MockChatBotScreen({ route }) {
   return <Text>ChatBot Screen: {route.params?.q}</Text>;
