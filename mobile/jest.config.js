d024d3c8f192ecb4e65e3497c63639f239a91638
lkay21 <115806181+lkay21@users.noreply.github.com>
2026-03-25 21:03:08 -0400

    update for mobile sync

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "jest-expo",
   setupFiles: [
-    "../jestSetup.js"
+    "./jestSetup.js"
   ],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|react-native-svg))",
