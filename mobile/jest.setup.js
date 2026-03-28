2b32d6a19414653218e45afb97d3c1b9849a79c5
Aryan Srivastava <aryansrivastava@Aryans-MacBook-Pro.local>
2026-03-28 18:52:46 -0400

    calender mapping complete:

--- mobile/jest.setup.js
+++ mobile/jest.setup.js
@@ -1,9 +1,12 @@
 import '@testing-library/jest-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
 
-jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
+// DashboardScreen and api.js import AsyncStorage; Jest has no native module without this mock.
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
 
 jest.mock('expo-linear-gradient', () => {
 	const React = require('react');
