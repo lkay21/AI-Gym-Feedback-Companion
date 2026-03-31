049f909363f66fd01ec79ebcea8e3d22a3414227
aryansri0208 <aryansrivastava@Aryans-Mac-mini.local>
2026-03-31 15:20:42 -0400

    Mobile tests fix

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "jest-expo",
   setupFiles: [
     "./jestSetup.js"
-  ],7
+  ],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|react-native-svg))",
   ],
